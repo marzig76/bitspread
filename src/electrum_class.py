7aa814cc7689d25ab4ed6b3a171460add6f4075b
Mike M <marzig76@gmail.com>
2015-08-18 22:04:48 -0400

    continue working on the electrum api class.. finished the getrxaddress functionailty

--- src/electrum_class.py
+++ src/electrum_class.py
@@ -1,3 +1,4 @@
+import json
 from executor import execute
 
 class electrumapi:
@@ -21,11 +22,21 @@ class electrumapi:
         return True
 
     # get a receiving address that hasn't been used yet
-    # this is not complete.. needs work
     def getrxaddress(self):
-        command = self.coinbinary + " FILL THIS IN"
-        output = execute(command, capture='True')
-        return output
+        # first get a list of available addresses
+        getaddresses = self.listaddresses()
+        addresslist = json.loads(getaddresses)
+
+        # for each address, check to see if it's ever been used before
+        for address in addresslist:
+            gethistory = self.getaddresshistory(address)
+            historylist = json.loads(gethistory)
+
+            # once we find an address that hasn't been used (history is empty), return that address
+            if historylist == []:
+                return address
+
+        return False
 
     # create and sign a bitcoin transaction
     # return the hex transaction to broadcast
@@ -42,7 +53,7 @@ class electrumapi:
     def broadcast(self, tx):
         if self.iscoinset():
             command = self.coinbinary + " broadcast " + tx
-            # is there output?
+            output = execute(command, capture='True')
             return True
         else:
             return False
@@ -51,5 +62,23 @@ class electrumapi:
     def iscoinset(self):
         if self.coinbinary != "":
             return True
+        else:
+            return False
+
+    # standard electrum command - lists addresses
+    def listaddresses(self):
+        if self.iscoinset():
+            command = self.coinbinary + " listaddresses"
+            output = execute(command, capture='True')
+            return output
+        else:
+            return False
+
+    # standard electrum command - gets the history of an address
+    def getaddresshistory(self, address):
+        if self.iscoinset():
+            command = self.coinbinary + " getaddresshistory " + address
+            output = execute(command, capture='True')
+            return output
         else:
             return False
