ec65603ec457d3b2cda6c401d9472ab45a1c2180
Mike M <marzig76@gmail.com>
2015-08-25 22:10:17 -0400

    test getlatestrate function

--- src/sqlite_examples.py
+++ src/sqlite_examples.py
@@ -1,22 +1,8 @@
-import sqlite3
+from sqlite_class import ratedata
 
 
-conn = sqlite3.connect('../db/rates.db')
-c = conn.cursor()
+datastore = ratedata()
 
-# Create table
-#c.execute('''CREATE TABLE rate (pair text, rate real, dtg text)''')
+print datastore.getlatestrate()
 
-# Insert a row of data
-#c.execute('delete from rate')
-#c.execute('INSERT INTO rate VALUES (?,?,datetime())', ('BTC_LTC',66.5656))
-
-for row in c.execute(''):
-    print row
-
-# Save (commit) the changes
-conn.commit()
-
-# We can also close the connection if we are done with it.
-# Just be sure any changes have been committed or they will be lost.
-conn.close()
+datastore.dataclose()
