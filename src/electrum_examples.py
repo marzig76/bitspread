d74bb1d3dbc3e144c96d27c33acab56a56e0d247
Mike M <marzig76@gmail.com>
2015-08-25 22:11:45 -0400

    tested out getbalance and daemon controls

--- src/electrum_examples.py
+++ src/electrum_examples.py
@@ -2,17 +2,25 @@ import json
 from electrum_class import electrumapi
 
 # create wallet instance
-bitcoinwallet = electrumapi('btc')
+#bitcoinwallet = electrumapi('btc')
+
+
+# start daemon
+#print bitcoinwallet.daemon('start')
+
+# get balance
+#balance = bitcoinwallet.getbalance()
+#print balance
 
 # get a rx address
-address = bitcoinwallet.getrxaddress()
+#address = bitcoinwallet.getrxaddress()
 
 # mktx
-testtx = bitcoinwallet.mktx(address, "0.00543569")
-jsontx = json.loads(testtx)
-print testtx, jsontx['hex']
+#testtx = bitcoinwallet.mktx(address, "0.00543569")
+#jsontx = json.loads(testtx)
+#print testtx, jsontx['hex']
 
 # broadcast
 #result = bitcoinwallet.broadcast(jsontx['hex'])
 
-print result
+#print result
