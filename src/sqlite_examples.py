dc6f0d140216a67a81f557d58690b459e8768b32
Mike M <marzig76@gmail.com>
2015-08-26 22:06:14 -0400

    tested out new functions

--- src/sqlite_examples.py
+++ src/sqlite_examples.py
@@ -3,6 +3,21 @@ from sqlite_class import ratedata
 
 datastore = ratedata()
 
-print datastore.getlatestrate()
+
+
+# insert rate
+#datastore.rateinsert("test", 99.99)
+
+# insert trade
+#datastore.tradeinsert("test", 99.99, 1.5, "1zasdfe", "L3dasda")
+
+# get latest rate
+#print "rate", datastore.getlatestrate()
+
+# get latest trade
+result = datastore.getlatesttrade()
+print result['pair'], result['rate'], result['amount'], result['src_addr'], result['dst_addr']
+
+#print pair, rate, amount, src_addr, dst_addr
 
 datastore.dataclose()
