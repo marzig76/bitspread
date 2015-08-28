8655f377a12f8dabb5f3a7db51fd3a860f4de4c3
Mike M <marzig76@gmail.com>
2015-08-27 22:19:02 -0400

    just some inital playing around.. still need to develop logic for this

--- src/bittrader.py
+++ src/bittrader.py
@@ -3,5 +3,31 @@ import time
 import requests
 from sqlite_class import ratedata
 from electrumwrapper import electrumapi
+from shapeshiftwrapper import exchangeapi
 
-# Get last trade
+# initialize datastore
+datastore = ratedata()
+
+# initialize the crypto wrappers
+bitcoinwallet = electrumapi('btc')
+litecoinwallet = electrumapi('ltc')
+
+# define trade api variables
+exchange = exchangeapi()
+
+# enter service loop
+
+while (True):
+    # Get last rate
+    #latestrate = datastore.getlatestrate()
+
+    # Get latest trade
+    #latesttrade = datastore.getlatesttrade()
+
+    # work on this logic..    
+    newaddress = litecoinwallet.getrxaddress()
+    depositaddress = exchange.getdepositaddress(newaddress, "btc_ltc")
+
+    print "final response: ", depositaddress
+
+    time.sleep(59)
